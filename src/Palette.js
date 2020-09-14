a33da3e01343172bc55215fc52c0b7134bdda6ba
luckster8089 <ly.lucky88@gmail.com>
2020-09-14 13:44:52 -0500

    Added styling to Palette

--- src/Palette.js
+++ src/Palette.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import ColorBox from './ColorBox'
+import './Palette.css'
 
-export default function Palette() {
+export default function Palette({ colors }) {
+    const colorBoxes = colors.map(color => (
+        <ColorBox background={color.color} name={color.name} />
+    ))
+
     return (
         <div className="Palette">
         {/* Navbar goes here */}
             <div className="Palette-colors">
-            {/* bunch of color boxes */}
+            {colorBoxes}
             </div>
-            {/* Footer eventually goes here */}
+        {/* Footer eventually goes here */}
         </div>
     )
 }
