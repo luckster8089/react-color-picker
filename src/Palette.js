038d38fd95f8c65bf485c42273a74ac9ae3d99cb
luckster8089 <ly.lucky88@gmail.com>
2020-09-15 16:04:44 -0500

    Style Slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import ColorBox from './ColorBox';
+import 'rc-slider/assets/index.css';
 import './Palette.css';
 import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
+
 
 export default function Palette({ palette }) {
     const [level, setLevel] = useState(500)
@@ -18,13 +19,15 @@ export default function Palette({ palette }) {
 
     return (
         <div className="Palette">
-        <Slider 
-            defaultValue={level} 
-            min={100} 
-            max={900}
-            step={100} 
-            onAfterChange={changeLevel} 
-        />
+            <div className="slider">
+                <Slider 
+                    defaultValue={level} 
+                    min={100} 
+                    max={900}
+                    step={100} 
+                    onAfterChange={changeLevel} 
+                />
+            </div>
         {/* Navbar goes here */}
             <div className="Palette-colors">
             {colorBoxes}
