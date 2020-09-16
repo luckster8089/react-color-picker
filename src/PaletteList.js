850e6dd7a363c6959f00794c4ddea574238c748b
luckster8089 <ly.lucky88@gmail.com>
2020-09-16 16:45:20 -0500

    Start styling PaletteList withStyles

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,13 +1,52 @@
-import React from 'react'
-import MiniPalette from './MiniPalette'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import MiniPalette from './MiniPalette';
+import { withStyles } from '@material-ui/styles';
 
-export default function PaletteList({ palettes }) {
+const styles = {
+    root: {
+        backgroundColor: "blue",
+        height: "100%",
+        display: "flex",
+        alignItems: "flex-start",
+        justifyContent: "center"
+    },
+    container: {
+        width: "50%",
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection: "column",
+        flexWrap: "wrap"
+    },
+    nav: {
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-between",
+        color: "white"
+    },
+    palettes: {
+        boxSizing: "border-box",
+        width: "100%",
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 30%)",
+        gridGap: "5%"
+    }
+}
+
+function PaletteList({ classes, palettes }) {
     return (
-        <div>
-            {palettes.map(palette => (
+        <div className={classes.root}>
+            <div className={classes.container}>
+                <nav className={classes.nav}>
+                    <h1>Pick Your Colors!</h1>
+                </nav>
+                <div className={classes.palettes}>
+                {palettes.map(palette => (
                 <MiniPalette {...palette} />
-            ))}
+                ))}
+                </div>
+            </div>
         </div>
     )
 }
+
+export default withStyles(styles)(PaletteList)
