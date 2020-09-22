1bd75ce52e29be8abc099dfb2143b067e6e43956
luckster8089 <ly.lucky88@gmail.com>
2020-09-22 17:38:01 -0500

    Move out styles

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,39 +1,10 @@
 import React from 'react';
 import MiniPalette from './MiniPalette';
 import { withStyles } from '@material-ui/styles';
+import styles from './styles/PaletteListStyles'
 
-const styles = {
-    root: {
-        backgroundColor: "blue",
-        height: "100vh",
-        display: "flex",
-        alignItems: "flex-start",
-        justifyContent: "center"
-    },
-    container: {
-        width: "50%",
-        display: "flex",
-        alignItems: "flex-start",
-        flexDirection: "column",
-        flexWrap: "wrap"
-    },
-    nav: {
-        display: "flex",
-        width: "100%",
-        justifyContent: "space-between",
-        color: "white"
-    },
-    palettes: {
-        boxSizing: "border-box",
-        width: "100%",
-        display: "grid",
-        gridTemplateColumns: "repeat(3, 30%)",
-        gridGap: "5%"
-    }
-}
 
 function PaletteList({ classes, palettes }) {
-
     return (
         <div className={classes.root}>
             <div className={classes.container}>
